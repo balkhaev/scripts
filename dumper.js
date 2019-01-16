c66437eba385a868b6600aaaeee4ee3fb4b73f53
Balkhaev <m.balkhaev@gmail.com>
2019-01-16 13:41:22 +0300

    Update dumper script

--- dumper.js
+++ dumper.js
@@ -1,26 +1,28 @@
 const fs = require('fs');
 
-module.exports = ({ dumpFile } = {}) => {
-  const dumpFilePath = __dirname + dumpFile;
+module.exports = ({ filename } = {}) => {
+  const filepath = __dirname + filename;
+
   let dump = { data: {}, code: null, errno: null, message: null };
 
-  if (fs.existsSync(dumpFilePath)) {
+  if (fs.existsSync(filepath)) {
     try {
-      dump = JSON.parse(fs.readFileSync(dumpFilePath));
+      dump = JSON.parse(fs.readFileSync(filepath));
     } catch(e) {
       console.error('Invalid dump file');
     } finally {
-      fs.unlinkSync(dumpFilePath);
+      fs.unlinkSync(filepath);
     }
   }
 
   process.on('uncaughtException', function (err) {
     const { code, errno, message } = err;
+
     dump.code = code;
     dump.errno = errno;
     dump.message = message;
 
-    fs.writeFileSync(dumpFilePath, JSON.stringify(dump, null, 2));
+    fs.writeFileSync(filepath, JSON.stringify(dump, null, 2));
 
     throw err;
   });
